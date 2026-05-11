188c83e96505c85cd790db1f28d2acb8267885e1
Shubhodeep <mukherjeeshubhodeep9@gmail.com>
2026-05-12 00:17:04 +0530

    Added the RAG Retrieval pipeline

--- ingestion_pipeline.py
+++ ingestion_pipeline.py
@@ -1,9 +1,12 @@
 import os
 from langchain_community.document_loaders import TextLoader, DirectoryLoader
 from langchain_text_splitters import CharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings
+# from langchain_openai import OpenAIEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 from dotenv import load_dotenv
+
+load_dotenv()
 
 def load_documents(docs_path="docs"):
     """Load all text files from the docs directory"""
@@ -27,12 +30,12 @@ def load_documents(docs_path="docs"):
         raise FileNotFoundError(f"No .txt files found in {docs_path}. Please add your company documents.")
     
    
-    for i, doc in enumerate(documents[:2]):  # Show first 2 documents
-        print(f"\nDocument {i+1}:")
-        print(f"  Source: {doc.metadata['source']}")
-        print(f"  Content length: {len(doc.page_content)} characters")
-        print(f"  Content preview: {doc.page_content[:100]}...")
-        print(f"  metadata: {doc.metadata}")
+    # for i, doc in enumerate(documents[:2]):  # Show first 2 documents
+    #     print(f"\nDocument {i+1}:")
+    #     print(f"  Source: {doc.metadata['source']}")
+    #     print(f"  Content length: {len(doc.page_content)} characters")
+    #     print(f"  Content preview: {doc.page_content[:100]}...")
+    #     print(f"  metadata: {doc.metadata}")
 
     return documents
 
@@ -47,18 +50,18 @@ def split_documents(documents, chunk_size=1000, chunk_overlap=0):
     
     chunks = text_splitter.split_documents(documents)
     
-    if chunks:
+    # if chunks:
     
-        for i, chunk in enumerate(chunks[:5]):
-            print(f"\n--- Chunk {i+1} ---")
-            print(f"Source: {chunk.metadata['source']}")
-            print(f"Length: {len(chunk.page_content)} characters")
-            print(f"Content:")
-            print(chunk.page_content)
-            print("-" * 50)
+    #     for i, chunk in enumerate(chunks[:5]):
+    #         print(f"\n--- Chunk {i+1} ---")
+    #         print(f"Source: {chunk.metadata['source']}")
+    #         print(f"Length: {len(chunk.page_content)} characters")
+    #         print(f"Content:")
+    #         print(chunk.page_content)
+    #         print("-" * 50)
         
-        if len(chunks) > 5:
-            print(f"\n... and {len(chunks) - 5} more chunks")
+    #     if len(chunks) > 5:
+    #         print(f"\n... and {len(chunks) - 5} more chunks")
     
     return chunks
 
@@ -66,7 +69,8 @@ def create_vector_store(chunks, persist_directory="db/chroma_db"):
     """Create and persist ChromaDB vector store"""
     print("Creating embeddings and storing in ChromaDB...")
         
-    embedding_model = OpenAIEmbeddings(model="text-embedding-3-small")
+    # embedding_model = OpenAIEmbeddings(model="text-embedding-3-small")
+    embedding_model = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
     
     # Create ChromaDB vector store
     print("--- Creating vector store ---")
@@ -91,6 +95,10 @@ def main():
     chunks = split_documents(documents)
     
     #3: Embedding and Storing in Chroma DB
+    vectorstore = create_vector_store(chunks)
+
+    print("\n✅ Ingestion complete! Your documents are now ready for RAG queries.")
+    return vectorstore
 
 if __name__ == "__main__":
     main()
